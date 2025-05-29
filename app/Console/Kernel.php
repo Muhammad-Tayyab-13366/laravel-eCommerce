2ff1869effaaf93687b77ed87bc225723914b692
mtayyab1366 <mtayyab1366@gmail.com>
2025-05-29 18:03:48 +0500

    stop cron job 'write-date-time'

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('write-date-to-file')->everyMinute();
+        //$schedule->command('write-date-to-file')->everyMinute();
     }
 
     /**
