cb91606a81afbd544746746a31d3f09c01b9b871
mtayyab1366 <mtayyab1366@gmail.com>
2025-06-02 18:23:40 +0500

    add link of login register to top

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -60,7 +60,8 @@
                         @if (Auth::check())
                             <a href="{{ route('account.profile') }}" class="nav-link text-dark">My Account</a>
                         @else 
-                            <a href="{{ route('account.login') }}" class="nav-link text-dark">Login/Register</a>
+                            <a href="{{ route('account.login') }}" class="nav-link text-dark">Login</a> / 
+                            <a href="{{ route('account.register') }}" class="nav-link text-dark">Login/Register</a>
                         @endif
                         
                         <form action="">					
